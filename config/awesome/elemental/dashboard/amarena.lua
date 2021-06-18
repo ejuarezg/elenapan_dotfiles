aac6b8f02af2cf074fa13e53fcbb46fb923ab050
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2021-06-18 11:38:04 -0400

    Bug fix: Obtain host name without async shell
    The use of async caused `host_text.markup` to never be updated.

--- config/awesome/elemental/dashboard/amarena.lua
+++ config/awesome/elemental/dashboard/amarena.lua
@@ -109,11 +109,7 @@ user_text.align = "center"
 user_text.valign = "center"
 
 local host_text = wibox.widget.textbox()
-awful.spawn.easy_async_with_shell("hostname", function(out)
-    -- Remove trailing whitespaces
-    out = out:gsub('^%s*(.-)%s*$', '%1')
-    host_text.markup = helpers.colorize_text("@"..out, x.color8)
-end)
+host_text.markup = helpers.colorize_text("@"..awesome.hostname, x.color8)
 -- host_text.markup = "<span foreground='" .. x.color8 .."'>" .. minutes.text .. "</span>"
 host_text.font = "monospace 16"
 host_text.align = "center"
